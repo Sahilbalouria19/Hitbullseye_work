d72b4a9edd5366810bc2cfd92deb06fe28e0fce8
sahilbalouria19 <sahilbalouria19@gmail.com>
2023-01-27 18:50:58 +0530

    file added

---
+++ first/src/ClassCop.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class ClassCop extends Component {
+  render() {
+    return (
+      <div><h1>ClassCop</h1></div>
+    )
+  }
+}
---
+++ first/src/Func.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Func() {
+  return (
+    <div>
+        <h1>Hello Sam</h1></div>
+  )
+}
