ecb6edd899122125201dc4ff9428bbeae410c85d
sahilbalouria19 <sahilbalouria19@gmail.com>
2023-01-27 20:51:34 +0530

    file added

--- arrowfunction.js
+++ arrowfunction.js
@@ -1,6 +1,13 @@
 //arrow func
 
-let add = (a,b) => a+b
+// let add = (a,b) => a+b
 
-let result = add(2+6)
-console.log(result)
+// let result = add(2+6)
+// console.log(result)
+
+
+//Refular function
+let add = function(a,b) {
+    console.log()
+}
+add(5,7,3,4)
